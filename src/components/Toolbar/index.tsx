141b0bb6f1a2ea274bf2a0b7341fbcd8ff665ead
shmck <shawn.j.mckay@gmail.com>
2018-07-29 18:33:45 -0700

    clenaup toolbar icons

--- src/components/Toolbar/index.tsx
+++ src/components/Toolbar/index.tsx
@@ -26,87 +26,101 @@ class Toolbar extends React.Component<IProps> {
 	render() {
 		return (
 			<div id="toolbar">
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="//at.alicdn.com/t/font_598462_3xve1872wizzolxr.css"
-				/>
-				<i
-					data-command="undo"
-					className="command iconfont icon-undo"
-					title="undo"
-				/>
-				<i
-					data-command="redo"
-					className="command iconfont icon-redo"
-					title="redo"
-				/>
-				<span className="separator" />
-				<i
-					data-command="copy"
-					className="command iconfont icon-copy-o"
-					title="copy"
-				/>
-				<i
-					data-command="paste"
-					className="command iconfont icon-paster-o"
-					title="paste"
-				/>
-				<i
-					data-command="delete"
-					className="command iconfont icon-delete-o"
-					title="delete"
-				/>
-				<span className="separator" />
-				<i
-					data-command="zoomIn"
-					className="command iconfont icon-zoom-in-o"
-					title="zoom in"
-				/>
-				<i
-					data-command="zoomOut"
-					className="command iconfont icon-zoom-out-o"
-					title="zoom out"
-				/>
-				<i
-					data-command="autoZoom"
-					className="command iconfont icon-fit"
-					title="auto zoom"
-				/>
-				<i
-					data-command="resetZoom"
-					className="command iconfont icon-actual-size-o"
-					title="reset zoom"
-				/>
-				<span className="separator" />
-				<i
-					data-command="toBack"
-					className="command iconfont icon-to-back"
-					title="to back"
-				/>
-				<i
-					data-command="toFront"
-					className="command iconfont icon-to-front"
-					title="to front"
-				/>
-				<span className="separator" />
-				<i
-					data-command="multiSelect"
-					className="command iconfont icon-select"
-					title="multi select"
-				/>
-				<i
-					data-command="addGroup"
-					className="command iconfont icon-group"
-					title="group"
-				/>
-				<i
-					data-command="unGroup"
-					className="command iconfont icon-ungroup"
-					title="ungroup"
-				/>
-				<Icon type="save" className="iconfont" onClick={this.save} />
-				<Icon type="export" className="iconfont" onClick={this.export} />
+				<span>
+					<link
+						rel="stylesheet"
+						type="text/css"
+						href="//at.alicdn.com/t/font_598462_3xve1872wizzolxr.css"
+					/>
+					<i
+						data-command="undo"
+						className="command iconfont icon-undo"
+						title="undo"
+					/>
+					<i
+						data-command="redo"
+						className="command iconfont icon-redo"
+						title="redo"
+					/>
+					<span className="separator" />
+					<i
+						data-command="copy"
+						className="command iconfont icon-copy-o"
+						title="copy"
+					/>
+					<i
+						data-command="paste"
+						className="command iconfont icon-paster-o"
+						title="paste"
+					/>
+					<i
+						data-command="delete"
+						className="command iconfont icon-delete-o"
+						title="delete"
+					/>
+					<span className="separator" />
+					<i
+						data-command="zoomIn"
+						className="command iconfont icon-zoom-in-o"
+						title="zoom in"
+					/>
+					<i
+						data-command="zoomOut"
+						className="command iconfont icon-zoom-out-o"
+						title="zoom out"
+					/>
+					<i
+						data-command="autoZoom"
+						className="command iconfont icon-fit"
+						title="auto zoom"
+					/>
+					<i
+						data-command="resetZoom"
+						className="command iconfont icon-actual-size-o"
+						title="reset zoom"
+					/>
+					<span className="separator" />
+					<i
+						data-command="toBack"
+						className="command iconfont icon-to-back"
+						title="to back"
+					/>
+					<i
+						data-command="toFront"
+						className="command iconfont icon-to-front"
+						title="to front"
+					/>
+					<span className="separator" />
+					<i
+						data-command="multiSelect"
+						className="command iconfont icon-select"
+						title="multi select"
+					/>
+					<i
+						data-command="addGroup"
+						className="command iconfont icon-group"
+						title="group"
+					/>
+					<i
+						data-command="unGroup"
+						className="command iconfont icon-ungroup"
+						title="ungroup"
+					/>
+				</span>
+
+				<span
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						padding: '0 2rem',
+					}}>
+					<Icon type="save" onClick={this.save} />
+					<Icon
+						type="export"
+						style={{ marginLeft: '2rem' }}
+						onClick={this.export}
+					/>
+				</span>
 			</div>
 		)
 	}
