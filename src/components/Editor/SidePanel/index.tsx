01a6fb88f03d28f672a36da43bcc23658ceee2a6
shmck <shawn.j.mckay@gmail.com>
2018-08-15 23:08:34 -0700

    add tab bar navigation to side panel

---
+++ src/components/Editor/SidePanel/index.tsx
@@ -0,0 +1,126 @@
+import { Tabs } from 'antd'
+import * as React from 'react'
+
+import Navigator from '../../Navigator'
+import * as Details from '../Details'
+import Zoom from '../Zoom'
+
+const TabPane = Tabs.TabPane
+
+interface IProps {
+	editor: any
+	flow: any
+	model: any
+	onChange(change: any): void
+}
+
+export default class SidePanel extends React.Component<IProps> {
+	toggleGrid = (ev: any) => {
+		if (ev.target.checked) {
+			this.props.flow.showGrid()
+		} else {
+			this.props.flow.hideGrid()
+		}
+	}
+	updateGraph = (key: string, value: any) => {
+		this.props.editor.executeCommand(() => {
+			const selectedItems = this.props.flow.getSelected()
+			selectedItems.forEach((item: any) => {
+				const updateModel = {}
+				updateModel[key] = value
+				this.props.flow.update(item, updateModel)
+			})
+		})
+	}
+	render() {
+		return (
+			<div id="sidepannel">
+				<Tabs
+					type="line"
+					size="small"
+					defaultActiveKey="1"
+					onChange={console.log}>
+					<TabPane tab="Details" key="1">
+						<div
+							style={{
+								display: 'flex',
+								flexDirection: 'column',
+								height: window.innerHeight - 82,
+								marginTop: '-15px',
+								justifyContent: 'space-between',
+							}}>
+							<div id="detailpannel">
+								<div
+									data-status="node-selected"
+									className="pannel"
+									id="node_detailpannel">
+									<div className="pannel-title">State</div>
+									<div className="block-container">
+										<Details.State
+											model={this.props.model}
+											onChange={this.props.onChange}
+											updateGraph={this.updateGraph}
+										/>
+									</div>
+								</div>
+								<div
+									data-status="edge-selected"
+									className="pannel"
+									id="edge_detailpannel">
+									<div className="pannel-title">Transition</div>
+									<div className="block-container">
+										<Details.Transition
+											model={this.props.model}
+											onChange={this.props.onChange}
+											updateGraph={this.updateGraph}
+										/>
+									</div>
+								</div>
+								<div
+									data-status="group-selected"
+									className="pannel"
+									id="node_detailpannel">
+									<div className="pannel-title">Group</div>
+									<div className="block-container">
+										<Details.Group
+											model={this.props.model}
+											onChange={this.props.onChange}
+											updateGraph={this.updateGraph}
+										/>
+									</div>
+								</div>
+								<div
+									data-status="canvas-selected"
+									className="pannel"
+									id="canvas_detailpannel">
+									<div className="pannel-title">Canvas</div>
+									<div className="block-container">
+										<Details.Canvas toggleGrid={this.toggleGrid} />
+									</div>
+								</div>
+							</div>
+							<div style={{ alignSelf: 'flex-end' }}>
+								<Zoom changeZoom={(zoom: number) => this.props.flow.zoom(zoom)}>
+									{({ zoom, minZoom, maxZoom, changeZoom }: any) => (
+										<Navigator
+											zoom={zoom}
+											minZoom={minZoom}
+											maxZoom={maxZoom}
+											changeZoom={changeZoom}
+										/>
+									)}
+								</Zoom>
+							</div>
+						</div>
+					</TabPane>
+					<TabPane tab="Nav" key="2">
+						<div>Navigate</div>
+					</TabPane>
+					<TabPane tab="JSON" key="3">
+						<div>JSON</div>
+					</TabPane>
+				</Tabs>
+			</div>
+		)
+	}
+}
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -9,13 +9,12 @@ import { items } from './RegisterFlow/PanelItems'
 
 import { load } from '../../utils/storage'
 import ContextMenu from '../ContextMenu'
-import Navigator from '../Navigator'
 import Page from '../Page'
 import Toolbar from '../Toolbar'
-import * as Details from './Details'
+import SidePanel from './SidePanel'
+
 import initEditor from './Editor'
 import initFlow from './Flow'
-import Zoom from './Zoom'
 
 interface IState {
 	selectedModel: any
@@ -34,6 +33,7 @@ class Editor extends React.Component<{}, IState> {
 		this.editor = initEditor(flow)
 		this.flow = flow
 		registerFlow()
+		this.forceUpdate()
 		// load saved data
 		this.load()
 	}
@@ -48,24 +48,6 @@ class Editor extends React.Component<{}, IState> {
 		this.flow.read(loaded)
 	}
 
-	toggleGrid = (ev: any) => {
-		if (ev.target.checked) {
-			this.flow.showGrid()
-		} else {
-			this.flow.hideGrid()
-		}
-	}
-	updateGraph = (key: string, value: any) => {
-		this.editor.executeCommand(() => {
-			const selectedItems = this.flow.getSelected()
-			selectedItems.forEach((item: any) => {
-				const updateModel = {}
-				updateModel[key] = value
-				this.flow.update(item, updateModel)
-			})
-		})
-	}
-
 	onChange = (change: any) => {
 		this.setState(change)
 	}
@@ -78,66 +60,12 @@ class Editor extends React.Component<{}, IState> {
 				<div style={{ height: '42px' }} />
 				<div className="bottom-container">
 					<ContextMenu />
-					<div id="detailpannel">
-						<div
-							data-status="node-selected"
-							className="pannel"
-							id="node_detailpannel">
-							<div className="pannel-title">State</div>
-							<div className="block-container">
-								<Details.State
-									model={model}
-									onChange={this.onChange}
-									updateGraph={this.updateGraph}
-								/>
-							</div>
-						</div>
-						<div
-							data-status="edge-selected"
-							className="pannel"
-							id="edge_detailpannel">
-							<div className="pannel-title">Transition</div>
-							<div className="block-container">
-								<Details.Transition
-									model={model}
-									onChange={this.onChange}
-									updateGraph={this.updateGraph}
-								/>
-							</div>
-						</div>
-						<div
-							data-status="group-selected"
-							className="pannel"
-							id="node_detailpannel">
-							<div className="pannel-title">Group</div>
-							<div className="block-container">
-								<Details.Group
-									model={model}
-									onChange={this.onChange}
-									updateGraph={this.updateGraph}
-								/>
-							</div>
-						</div>
-						<div
-							data-status="canvas-selected"
-							className="pannel"
-							id="canvas_detailpannel">
-							<div className="pannel-title">Canvas</div>
-							<div className="block-container">
-								<Details.Canvas toggleGrid={this.toggleGrid} />
-							</div>
-						</div>
-					</div>
-					<Zoom changeZoom={(zoom: number) => this.flow.zoom(zoom)}>
-						{({ zoom, minZoom, maxZoom, changeZoom }: any) => (
-							<Navigator
-								zoom={zoom}
-								minZoom={minZoom}
-								maxZoom={maxZoom}
-								changeZoom={changeZoom}
-							/>
-						)}
-					</Zoom>
+					<SidePanel
+						flow={this.flow}
+						onChange={this.onChange}
+						editor={this.editor}
+						model={model}
+					/>
 					<Page />
 				</div>
 			</div>
